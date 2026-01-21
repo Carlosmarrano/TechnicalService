2e5ff38d96f244b3bec654599bc94e1088a05fd9
Carlos Caguana <marranitocarlitos@gmail.com>
2026-01-20 22:22:09 -0400

    ContactPage actualizado

--- src/TechnicalService/pages/contactPage.jsx
+++ src/TechnicalService/pages/contactPage.jsx
@@ -5,18 +5,23 @@ import CallIcon from '@mui/icons-material/Call';
 import EmailIcon from '@mui/icons-material/Email';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import TelegramIcon from '@mui/icons-material/Telegram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import XIcon from '@mui/icons-material/X';
+import RedditIcon from '@mui/icons-material/Reddit';
+import InstagramIcon from '@mui/icons-material/Instagram';
+
 
 export const ContactPage = () => {
   return (
     <TechnicalLayout>
-      <Grid container sx={{ marginTop: 20, display: "flex", alignItems: "center", justifyContent: "center", flexDirection: { xs: "column", md: "row" } }}>
+      <Grid container sx={{ marginTop: 20, display: "flex", alignItems: "center", justifyContent: "center", flexDirection: { xs: "column", md: "row" }}}>
         <Box>
-          <Typography variant="h1" sx={{ textAlign: "center", fontSize: { xs: 40, md: 60, lg: 80 }, fontWeight: 500, marginRight: 10 }}>
+          <Typography variant="h1" sx={{ textAlign: "center", fontSize: { xs: 40, md: 60, lg: 80 }, fontWeight: 500, marginRight: 10.6 }}>
             Contáctanos
           </Typography>
         </Box>
 
-        <Grid container sx={{ marginTop: 10, display: "flex", flexDirection: { xs: "column", md: "row" }, justifyContent: "center", gap: 8, marginBottom: { xs: 14 }, marginRight: { xs: 10, sm: 0 , md: 8, lg: 0 }  }}>
+        <Grid container sx={{ marginTop: 10, display: "flex", flexDirection: { xs: "column", md: "row" }, justifyContent: "center", gap: 8, marginBottom: { xs: 14 }, marginRight: { xs: 11, sm: 2.5, md: 8, lg: 0 }  }}>
 
           <Box sx={{ display: "flex", width: { xs: '230px', sm: '360px', md: '450px', lg: '50vh' }, height: { xs: '290px', sm: '400px', md: '50vh' }, border: "1px solid secondary.main", boxShadow: 4, padding: 4, borderRadius: 2, marginRight: {sm: 10, md: 0} }}>
             <Map style={{ width: '100%', height: '100%' }} defaultCenter={[41.902782, 12.496366]} defaultZoom={11}>
@@ -25,9 +30,9 @@ export const ContactPage = () => {
             </Map>
           </Box>
 
-          <Box sx={{ display: "flex", justifyContent: "center", marginLeft: {sm: 7.5 ,md: 6, lg: 0 }, border: "1px solid secondary.main", boxShadow: 4, padding: 4, borderRadius: 2, textAlign: "center", width: { xs: '230px', sm: '240px', md: '250px' }, height: { xs: '320px', sm: '400px', md: '50vh' }, marginRight: {md: 8, lg: 0} }}>
+          <Box sx={{ display: "flex", justifyContent: "center", marginLeft: {sm: 7.5 ,md: 6, lg: 0 }, border: "1px solid secondary.main", boxShadow: 4, padding: 4, borderRadius: 2, textAlign: "center", width: { xs: '230px', sm: '240px', md: '250px' }, height: { xs: '320px', sm: '400px', md: '50vh' }, marginRight: { md: 10, lg: 0} }}>
             <Box>
-              <Typography>
+              <Typography sx={{ fontWeight: "600"}}>
                 Información de contácto
               </Typography>
 
@@ -54,29 +59,29 @@ export const ContactPage = () => {
 
           </Box>
 
-          <Box sx={{ display: "flex", justifyContent: "center", textAlign: "center", marginLeft: {sm: 7.5,md: 6, lg: 0 }, marginRight: 10, border: "1px solid secondary.main", boxShadow: 4, padding: 4, borderRadius: 2, width: { xs: '230px', sm: '240px', md: '250px' }, height: { xs: '320px', sm: '400px', md: '50vh' },  marginRight: {md: 8, lg: 0} }}>
+          <Box sx={{ display: "flex", justifyContent: "center", textAlign: "center", marginLeft: {sm: 7.5,md: 6, lg: 0 }, marginRight: 10, border: "1px solid secondary.main", boxShadow: 4, padding: 4, borderRadius: 2, width: { xs: '230px', sm: '240px', md: '250px' }, height: { xs: '320px', sm: '400px', md: '50vh' },  marginRight: {md: 10, lg: 0} }}>
             <Box>
-              <Typography>
-                Información de contácto
+              <Typography sx={{ fontWeight: "600"}}>
+                Nuestras redes sociales
               </Typography>
 
               <Box sx={{ marginTop: 8, display: "flex", flexDirection: "column", gap: 2 }}>
 
                 <Box sx={{ display: "flex", justifyContent: "space-evenly" }}>
-                  <CallIcon />
-                  <Typography>Hola como estás</Typography>
+                  <FacebookIcon />
+                  <Typography>TechnicalCJ</Typography>
                 </Box>
                 <Box sx={{ display: "flex", justifyContent: "space-evenly" }}>
-                  <EmailIcon />
-                  <Typography>Hola como estás</Typography>
+                  <XIcon />
+                  <Typography>TechnicalCJOfficial</Typography>
                 </Box>
                 <Box sx={{ display: "flex", justifyContent: "space-evenly" }}>
-                  <WhatsAppIcon />
-                  <Typography>Hola como estás</Typography>
+                  <RedditIcon />
+                  <Typography>TechnicalCJService</Typography>
                 </Box>
                 <Box sx={{ display: "flex", justifyContent: "space-evenly" }}>
-                  <TelegramIcon />
-                  <Typography>Hola como estás</Typography>
+                  <InstagramIcon />
+                  <Typography>@TechnicalCJIG</Typography>
                 </Box>
 
               </Box>
