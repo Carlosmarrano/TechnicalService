d7bf625d59a92210d3e6ebe529723b199f19787a
Carlos Caguana <marranitocarlitos@gmail.com>
2026-02-10 16:41:26 -0400

    Smooth agregado al botón de más info

--- src/TechnicalService/components/Buttons/infoButton.jsx
+++ src/TechnicalService/components/Buttons/infoButton.jsx
@@ -1,10 +1,20 @@
 import {Box, Button } from "@mui/material"
+import {scroller} from "react-scroll"
 
 export const InfoButton = () => {
 
+        const handleScroll = () => {
+            scroller.scrollTo('myScrollToElement', {
+                duration: 1500,
+                delay: 100,
+                smooth: true,
+                offset: -90
+            });
+        }
+
     return (
         <Box sx={{ display: "flex", justifyContent: { xs: "center", md: "flex-start" }, marginTop: { lg: 18, md: 15, xs: 8 }, marginBottom: { xs: 8 } }}>
-            <Button variant="contained" sx={{ bgcolor: "primary.main", color: "black", padding: { xs: 1, md: 1.5 }, borderRadius: 5, fontSize: { xs: 14, md: 16 } }}>
+            <Button onClick={handleScroll} variant="contained" sx={{ bgcolor: "primary.main", color: "black", padding: { xs: 1, md: 1.5 }, borderRadius: 5, fontSize: { xs: 14, md: 16 } }}>
                 Más info
             </Button>
         </Box>
--- src/TechnicalService/pages/homePage.jsx
+++ src/TechnicalService/pages/homePage.jsx
@@ -17,6 +17,7 @@ import ConstructionIcon from '@mui/icons-material/Construction';
 import HeadsetMicIcon from '@mui/icons-material/HeadsetMic';
 import ComputerIcon from '@mui/icons-material/Computer';
 import LightbulbIcon from '@mui/icons-material/Lightbulb';
+import { Element } from "react-scroll";
 
 export const HomePage = () => {
 
@@ -55,41 +56,44 @@ export const HomePage = () => {
                 <ReviewsButton />
             </Grid>
 
-            <Grid container sx={{ marginTop: 20, display: "flex", alignItems: "center", flexDirection: "column", gap: 8, marginRight: 10 }}>
-                <Box>
-                    <Typography variant="h2" sx={{ textAlign: "center", fontSize: { xs: 40, sm: 50, lg: 60 } }}>
-                        Nuestros Servicios
-                    </Typography>
-                </Box>
+            <Element name="myScrollToElement">
+                <Grid container sx={{ marginTop: 20, display: "flex", alignItems: "center", flexDirection: "column", gap: 8, marginRight: 10 }}>
+                    <Box>
 
-                <Box sx={{ display: "flex", flexDirection: { xs: "column", lg: "row" }, justifyContent: "space-between", gap: 4, width: "100%" }}>
-                    <Box sx={{ flex: 1, border: "1px solid secondary.main", boxShadow: 4, padding: 4, borderRadius: 2, maxWidth: "100%", textAlign: "center", display: "flex", flexDirection: "column", alignItems: "center", gap: 2 }}>
-                        <ConstructionIcon sx={{ fontSize: 50, color: "#002D3E" }}/>
-                        <Typography variant="h5">Servicio Técnico</Typography>
-                        <Typography>Contamos con un equipo de especialistas altamente capacitados para brindar mantenimiento preventivo y correctivo, asegurando que tus activos operen siempre al 100%.</Typography>
+                        <Typography variant="h2" sx={{ textAlign: "center", fontSize: { xs: 40, sm: 50, lg: 60 } }}>
+                            Nuestros Servicios
+                        </Typography>
                     </Box>
 
-                    <Box sx={{ flex: 1, border: "1px solid secondary.main", boxShadow: 4, padding: 4, borderRadius: 2, maxWidth: "100%", textAlign: "center", display: "flex", flexDirection: "column", alignItems: "center", gap: 2 }}>
-                        <HeadsetMicIcon sx={{ fontSize: 50, color: "#002D3E" }}/>
-                        <Typography variant="h5">Atención al Cliente</Typography>
-                        <Typography>Tu tranquilidad es nuestra prioridad. Ofrecemos un canal de comunicación directo y eficiente para resolver dudas, gestionar solicitudes y brindarte acompañamiento constante.</Typography>
-                    </Box>
-                </Box>
+                    <Box sx={{ display: "flex", flexDirection: { xs: "column", lg: "row" }, justifyContent: "space-between", gap: 4, width: "100%" }}>
+                        <Box sx={{ flex: 1, border: "1px solid secondary.main", boxShadow: 4, padding: 4, borderRadius: 2, maxWidth: "100%", textAlign: "center", display: "flex", flexDirection: "column", alignItems: "center", gap: 2 }}>
+                            <ConstructionIcon sx={{ fontSize: 50, color: "#002D3E" }} />
+                            <Typography variant="h5">Servicio Técnico</Typography>
+                            <Typography>Contamos con un equipo de especialistas altamente capacitados para brindar mantenimiento preventivo y correctivo, asegurando que tus activos operen siempre al 100%.</Typography>
+                        </Box>
 
-                <Box sx={{ display: "flex", flexDirection: { xs: "column", lg: "row" }, justifyContent: "space-between", gap: 4, width: "100%" }}>
-                    <Box sx={{ flex: 1, border: "1px solid secondary.main", boxShadow: 4, padding: 4, borderRadius: 2, maxWidth: "100%", textAlign: "center", display: "flex", flexDirection: "column", alignItems: "center", gap: 2 }}>
-                        <ComputerIcon sx={{ fontSize: 50, color: "#002D3E" }}/>
-                        <Typography variant="h5">Soporte Técnico</Typography>
-                        <Typography>Asistencia especializada para resolver cualquier incidencia operativa de forma rápida, ya sea de manera presencial o remota, minimizando tiempos de inactividad.</Typography>
+                        <Box sx={{ flex: 1, border: "1px solid secondary.main", boxShadow: 4, padding: 4, borderRadius: 2, maxWidth: "100%", textAlign: "center", display: "flex", flexDirection: "column", alignItems: "center", gap: 2 }}>
+                            <HeadsetMicIcon sx={{ fontSize: 50, color: "#002D3E" }} />
+                            <Typography variant="h5">Atención al Cliente</Typography>
+                            <Typography>Tu tranquilidad es nuestra prioridad. Ofrecemos un canal de comunicación directo y eficiente para resolver dudas, gestionar solicitudes y brindarte acompañamiento constante.</Typography>
+                        </Box>
                     </Box>
-                    <Box sx={{ flex: 1, border: "1px solid secondary.main", boxShadow: 4, padding: 4, borderRadius: 2, maxWidth: "100%", textAlign: "center", display: "flex", flexDirection: "column", alignItems: "center", gap: 2 }}>
-                        <LightbulbIcon sx={{ fontSize: 50, color: "#002D3E" }}/>
-                        <Typography variant="h5">Consultoria</Typography>
-                        <Typography>Te asesoramos en la toma de decisiones estratégicas para optimizar tus recursos tecnológicos y operativos, adaptándonos a las necesidades específicas de tu proyecto.</Typography>
+
+                    <Box sx={{ display: "flex", flexDirection: { xs: "column", lg: "row" }, justifyContent: "space-between", gap: 4, width: "100%" }}>
+                        <Box sx={{ flex: 1, border: "1px solid secondary.main", boxShadow: 4, padding: 4, borderRadius: 2, maxWidth: "100%", textAlign: "center", display: "flex", flexDirection: "column", alignItems: "center", gap: 2 }}>
+                            <ComputerIcon sx={{ fontSize: 50, color: "#002D3E" }} />
+                            <Typography variant="h5">Soporte Técnico</Typography>
+                            <Typography>Asistencia especializada para resolver cualquier incidencia operativa de forma rápida, ya sea de manera presencial o remota, minimizando tiempos de inactividad.</Typography>
+                        </Box>
+                        <Box sx={{ flex: 1, border: "1px solid secondary.main", boxShadow: 4, padding: 4, borderRadius: 2, maxWidth: "100%", textAlign: "center", display: "flex", flexDirection: "column", alignItems: "center", gap: 2 }}>
+                            <LightbulbIcon sx={{ fontSize: 50, color: "#002D3E" }} />
+                            <Typography variant="h5">Consultoria</Typography>
+                            <Typography>Te asesoramos en la toma de decisiones estratégicas para optimizar tus recursos tecnológicos y operativos, adaptándonos a las necesidades específicas de tu proyecto.</Typography>
+                        </Box>
                     </Box>
-                </Box>
-                <AllServiceButton />
-            </Grid>
+                    <AllServiceButton />
+                </Grid>
+            </Element>
 
             <Grid container sx={{ marginTop: 20, display: "flex", alignItems: "center", flexDirection: "column", gap: 8, marginRight: 10 }}>
                 <Box>
@@ -107,7 +111,7 @@ export const HomePage = () => {
                 </Box>
             </Grid>
 
-            <ContactForm/>
+            <ContactForm />
 
             <Grid component="footer" sx={{ marginTop: 20 }}>
                 <Container maxWidth="lg">
