b5f81ae0007a9dc49214586cb88040071119cad8
Carlos Caguana <marranitocarlitos@gmail.com>
2026-03-02 17:48:28 -0400

    Contact Form Actualizado

--- src/TechnicalService/components/Buttons/FormButton.jsx
+++ src/TechnicalService/components/Buttons/FormButton.jsx
@@ -1,10 +1,14 @@
 import { Button, Grid } from "@mui/material"
+import { useFormContext } from "react-hook-form"
 
 
 export const FormButtom = () => {
+
+    const { formState: { isSubmitting } } = useFormContext();
+
     return (
         <Grid size={{ xs: 12 }}>
-            <Button type="submit" fullWidth variant="contained" sx={{ bgcolor: "primary.main", color: "black", padding: { xs: 1, md: 1.5 }, borderRadius: 5, fontSize: { xs: 14, md: 16 } }}>Enviar</Button>
+            <Button type="submit" disabled={isSubmitting} fullWidth variant="contained" sx={{ bgcolor: "primary.main", color: "black", padding: { xs: 1, md: 1.5 }, borderRadius: 5, fontSize: { xs: 14, md: 16 } }}>{isSubmitting ? "Enviando...": "Enviar"}</Button>
         </Grid>
     )
 }
--- src/TechnicalService/components/ContactForm.jsx
+++ src/TechnicalService/components/ContactForm.jsx
@@ -1,11 +1,19 @@
 import { Alert, Box, Card, CardContent, Grid, Snackbar, TextField, Typography } from "@mui/material"
 import { FormButtom } from "./Buttons/FormButton"
-import { Controller, useForm } from "react-hook-form"
+import { Controller, useForm, FormProvider } from "react-hook-form"
 import { useState } from "react"
 
 export const ContactForm = () => {
 
-    const { control, handleSubmit, reset } = useForm({
+    const [snackbar, setSnackbar] = useState({
+        open: false,
+        message: '',
+        severity: "success"
+    });
+    const sleep = (ms) => new Promise((resolve => setTimeout(resolve, ms)));
+
+
+    const methods = useForm({
         defaultValues: {
             Nombre: "",
             Apellido: "",
@@ -15,56 +23,83 @@ export const ContactForm = () => {
         },
     })
 
-    const onSubmit = () => {
-        setOpen(true);
-        reset();
-    };
+    const onSubmit = async (data) => {
 
-    const [open, setOpen] = useState(false);
+        const ACCESS_KEY = import.meta.env.VITE_WEB3FORMS_ACCESS_KEY
 
-    const handleClose = (event, reason) => {
-        if (reason === "clickaway") {
-            return;
+        try {
+            await sleep(3000);
+
+            const response = await fetch('https://api.web3forms.com/submit', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
+                body: JSON.stringify({
+                    ...data,
+                    access_key: ACCESS_KEY
+                })
+            });
+
+            const result = await response.json();
+
+           if(result.success) {
+            methods.reset();
+
+            setSnackbar({
+                open: true,
+                message: "Formulario enviado con éxito",
+                severity: "success"
+            });
+
+            } else {
+                throw new Error(result.message);
+            }
+
+        } catch (error) {
+            setSnackbar({
+                open: true,
+                message: "Hubo un problema: " + error.message,
+                severity: "error"
+            })
         }
-
-        setOpen(false);
     };
 
     return (
-        <Grid container sx={{ marginTop: 20, display: "flex", alignItems: "center", flexDirection: "column", gap: 4, marginRight: 10 }}>
-            <Box>
-                <Typography variant="h2" sx={{ textAlign: "center", fontSize: { xs: 40, sm: 50, lg: 60 } }}>
-                    Contáctanos
-                </Typography>
-            </Box>
-            <Card sx={{ backgroundColor: "secondary.main", maxWidth: 800, display: "flex", justifyContent: "center", alignContent: "center", boxShadow: 5, mx: "auto" }}>
-                <CardContent>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <Grid container spacing={1}>
-                            <Grid size={{ xs: 12, sm: 6 }}>
-                                <Controller name="Nombre" control={control} rules={{ required: "El nombre es obligatorio", maxLength: { value: 50, message: "Máximo 50 caracteres" }, minLength: { value: 3, message: "Nombre demasiado corto" } }} render={({ field, fieldState: { error } }) => (<TextField {...field} label="Nombre Completo" placeholder="Pedro Manuel" variant="outlined" fullWidth error={!!error} helperText={error ? error.message : null} />)} />
+        <FormProvider {...methods}>
+            <Grid container sx={{ marginTop: 20, display: "flex", alignItems: "center", flexDirection: "column", gap: 4, marginRight: 10 }}>
+                <Box>
+                    <Typography variant="h2" sx={{ textAlign: "center", fontSize: { xs: 40, sm: 50, lg: 60 } }}>
+                        Contáctanos
+                    </Typography>
+                </Box>
+                <Card sx={{ backgroundColor: "secondary.main", maxWidth: 800, display: "flex", justifyContent: "center", alignContent: "center", boxShadow: 5, mx: "auto" }}>
+                    <CardContent>
+                        <form onSubmit={methods.handleSubmit(onSubmit)}>
+                            <Grid container spacing={1}>
+                                <Grid size={{ xs: 12, sm: 6 }}>
+                                    <Controller name="Nombre" rules={{ required: "El nombre es obligatorio", maxLength: { value: 50, message: "Máximo 50 caracteres" }, minLength: { value: 3, message: "Nombre demasiado corto" } }} render={({ field, fieldState: { error } }) => (<TextField {...field} label="Nombre Completo" placeholder="Pedro Manuel" variant="outlined" fullWidth error={!!error} helperText={error ? error.message : null} />)} />
+                                </Grid>
+                                <Grid size={{ xs: 12, sm: 6 }}>
+                                    <Controller name="Apellido" rules={{ required: "El apellido es obligatorio", maxLength: { value: 50, message: "Máximo 50 caracteres" }, minLength: { value: 5, message: "Apellido demasiado corto" } }} render={({ field, fieldState: {
+                                        error } }) => (<TextField {...field} label="Apellido Completo" placeholder="Perez Dominguéz" variant="outlined" fullWidth error={!!error} helperText={error ? error.message : null} />)} />
+                                </Grid>
+                                <Grid size={{ xs: 12 }}>
+                                    <Controller name="Email" rules={{ required: "El correo es obligatorio", pattern: { value: /^[a-zA-Z0-9._%+-]+@gmail\.com$/, message: "Debes ingrear una cuenta de Gmail válida (@gmail.com)" } }} render={({ field, fieldState: { error } }) => (<TextField {...field} label="Correo Electrónico" placeholder="ejemplo@gmail.com" variant="outlined" fullWidth error={!!error} helperText={error ? error.message : null} />)} />
+                                </Grid>
+                                <Grid size={{ xs: 12 }}>
+                                    <Controller name="Mensaje" rules={{ required: "Dejar un mensaje", maxLength: { value: 10000, message: "Máximo diez mil caracteres" }, minLength: { value: 100, message: "Mensaje demasiado corto" } }} render={({ field, fieldState: { error } }) => (<TextField  {...field} label="Deja tu mensaje" placeholder="Hola quería contactarme con ustedes...." variant="outlined" fullWidth multiline rows={5} error={!!error} helperText={error ? error.message : null} />)} />
+                                </Grid>
+                                <FormButtom />
                             </Grid>
-                            <Grid size={{ xs: 12, sm: 6 }}>
-                                <Controller name="Apellido" control={control} rules={{ required: "El apellido es obligatorio", maxLength: { value: 50, message: "Máximo 50 caracteres" }, minLength: { value: 5, message: "Apellido demasiado corto" } }} render={({ field, fieldState: {
-                                    error } }) => (<TextField {...field} label="Apellido Completo" placeholder="Perez Dominguéz" variant="outlined" fullWidth error={!!error} helperText={error ? error.message : null} />)} />
-                            </Grid>
-                            <Grid size={{ xs: 12 }}>
-                                <Controller name="Email" control={control} rules={{ required: "El correo es obligatorio", pattern: { value: /^[a-zA-Z0-9._%+-]+@gmail\.com$/, message: "Debes ingrear una cuenta de Gmail válida (@gmail.com)" } }} render={({ field, fieldState: { error } }) => (<TextField {...field} label="Correo Electrónico" placeholder="ejemplo@gmail.com" variant="outlined" fullWidth error={!!error} helperText={error ? error.message : null} />)} />
-                            </Grid>
-                            <Grid size={{ xs: 12 }}>
-                                <Controller name="Mensaje" control={control} rules={{ required: "Dejar un mensaje", maxLength: { value: 10000, message: "Máximo diez mil caracteres" }, minLength: { value: 100, message: "Mensaje demasiado corto" } }} render={({ field, fieldState: { error } }) => (<TextField  {...field} label="Deja tu mensaje" placeholder="Hola quería contactarme con ustedes...." variant="outlined" fullWidth multiline rows={5} error={!!error} helperText={error ? error.message : null} />)} />
-                            </Grid>
-                            <FormButtom />
-                        </Grid>
-                    </form>
-                </CardContent>
-            </Card>
+                        </form>
+                    </CardContent>
+                </Card>
 
-            <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
-                <Alert onClose={handleClose} severity="success" variant="filled" sx={{ width: "100%"}}>
-                    Formulario Completado
-                </Alert>
-            </Snackbar>
-        </Grid>
+                <Snackbar open={snackbar.open} autoHideDuration={4000} onClose={() => { setSnackbar({ ...snackbar, open: false }) }}>
+                    <Alert onClose={() => setSnackbar({ ...snackbar, open: false })} severity={snackbar.severity} variant="filled" sx={{ width: "100%" }}>
+                        {snackbar.message}
+                    </Alert>
+                </Snackbar>
+            </Grid>
+        </FormProvider>
     )
 }
