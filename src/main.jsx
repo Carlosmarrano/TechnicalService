8cfe1102410e8351b0cfdf787f97994f2ae32f48
Carlos Caguana <marranitocarlitos@gmail.com>
2026-06-04 10:05:15 -0400

    store añadido

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import * as ReactDOM from 'react-dom/client';
 import './styles.css';
 import { TechnicalServiceApp } from './TechnicalServiceApp.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <TechnicalServiceApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <TechnicalServiceApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
