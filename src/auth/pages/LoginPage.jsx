b53f51c30d43a6f6795c7ecd49dccd33c23d341a
Carlos Caguana <marranitocarlitos@gmail.com>
2026-06-09 11:06:08 -0400

    AuthSlice & thunk agregado

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -2,33 +2,57 @@ import { TextField, Grid, Button, Link } from '@mui/material';
 import { Link as RouterLink } from "react-router-dom";
 import { Apple, Google } from "@mui/icons-material";
 import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from '../../hooks/useForm';
+import { useDispatch } from 'react-redux';
+import { checkingAuthentication, startGoogleSignIn } from '../../store/auth/thunks';
 
 export const LoginPage = () => {
+
+  const dispatch = useDispatch();
+
+  const { email, password, onInputChange } = useForm({
+    email: "",
+    password: ""  
+  });
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    dispatch(checkingAuthentication());
+  };
+
+  const onGoogleSignIn = () => {
+    dispatch(startGoogleSignIn());
+  }
+
   return (
-  <AuthLayout title='Login'>
-    <Grid container component="form" spacing={2}>
+    <AuthLayout title='Login'>
+
+      <form onSubmit={onSubmit}>
+        <Grid container spacing={2}>
           <Grid size={{ xs: 12 }}>
-            <TextField id="email"  label="Email"  type="email" placeholder="email@google.com" size="small" fullWidth/>
+            <TextField id="email" label="Email" type="email" size="small"  fullWidth name="email" value={email} onChange={onInputChange} />
           </Grid>
           <Grid size={{ xs: 12 }}>
-            <TextField id="password" label="Password" type="password" placeholder="password" size="small" fullWidth/>
+            <TextField id="password" label="Password" type="password" size="small" fullWidth name="password" value={password} onChange={onInputChange} />
           </Grid>
-    </Grid>
+        </Grid>
 
         <Grid container spacing={2} sx={{ mt: 2 }} >
           <Grid size={{ xs: 4 }}>
-            <Button variant="contained" fullWidth >Login</Button>
+            <Button variant="contained" fullWidth type="submit">Login</Button>
           </Grid>
-          <Grid size={{ xs: 4 }}> 
-            <Button variant="contained" sx={{height: "36.5px"}} fullWidth aria-label='Google' aria-labelledby='link' role='button' ><Google/></Button>
+          <Grid size={{ xs: 4 }}>
+            <Button variant="contained" sx={{ height: "36.5px" }} fullWidth aria-label='Google' aria-labelledby='link' role='button' onClick={ onGoogleSignIn} ><Google /></Button>
           </Grid>
-          <Grid size={{ xs: 4 }}> 
-            <Button variant="contained" sx={{height: "36.5px"}} fullWidth aria-label='Apple' aria-labelledby='link' role='button'><Apple/></Button>
+          <Grid size={{ xs: 4 }}>
+            <Button variant="contained" sx={{ height: "36.5px" }} fullWidth aria-label='Apple' aria-labelledby='link' role='button'><Apple /></Button>
           </Grid>
         </Grid>
         <Grid container direction="row" justifyContent="end" sx={{ mt: 2 }}>
           <Link component={RouterLink} color="inherit" to="/auth/register">Crea una cuenta</Link>
         </Grid>
-  </AuthLayout>
+      </form>
+    </AuthLayout>
   );
 };
